e6f8ff48820305447dbd70ef40bfb5d2db983087
chenxiaohua602 <1790248966@qq.com>
2016-06-14 22:46:22 +0800

    first commit

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,11 +1,18 @@
 'use strict';
 
 function double_to_one(collection) {
-  var b[], c = 0;
+  var b = [];
+  var c = 0;
   for (var i = 0; i < collection.length; i++)
-    for (var j = 0; j < collection[i].length; j++) {
+    if(collection[i].length)
+      for (var j = 0; j < collection[i].length; j++) {
       b[c] = collection[i][j];
       c++;
+    }
+    else
+    {
+      b[c] = collection[i];
+      c ++;
     }
   return b;}
   module.exports = double_to_one;
